5d1c1b7c7f08304f0503a9a577f9b74669a8f729
h-tko <bojan_krkic_esp@i.softbank.jp>
2017-01-17 09:09:55 +0000

    add access_log create.

---
+++ db/migrations/20170117083017_create_access_log.sql
@@ -0,0 +1,16 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+CREATE TABLE access_logs (
+    id SERIAL PRIMARY KEY NOT NULL,
+    ip_address text NOT NULL,
+    user_agent text NOT NULL,
+    created_at timestamp(0) not null default now(),
+    updated_at timestamp(0),
+    deleted_at timestamp(0)
+);
+
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+DROP TABLE access_logs
