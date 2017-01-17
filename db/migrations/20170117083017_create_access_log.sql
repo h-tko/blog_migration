8d8e410f0d9fcd5e52db167ff212fa87ec231fb6
h-tko <bojan_krkic_esp@i.softbank.jp>
2017-01-17 09:17:06 +0000

    uri追加

--- db/migrations/20170117083017_create_access_log.sql
+++ db/migrations/20170117083017_create_access_log.sql
@@ -5,6 +5,7 @@ CREATE TABLE access_logs (
     id SERIAL PRIMARY KEY NOT NULL,
     ip_address text NOT NULL,
     user_agent text NOT NULL,
+    uri text NOT NULL,
     created_at timestamp(0) not null default now(),
     updated_at timestamp(0),
     deleted_at timestamp(0)
@@ -13,4 +14,4 @@ CREATE TABLE access_logs (
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
-DROP TABLE access_logs
+DROP TABLE access_logs;
